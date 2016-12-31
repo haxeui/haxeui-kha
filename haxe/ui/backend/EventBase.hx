cf35a7be029a0e5096f0e8de2f223209135a0b45
Ian Harrigan <ianharrigan@hotmail.com>
2016-12-31 16:57:41 +0100

    add EventBase

---
+++ haxe/ui/backend/EventBase.hx
@@ -0,0 +1,14 @@
+package haxe.ui.backend;
+
+import haxe.ui.core.UIEvent;
+
+class EventBase {
+    public function new() {
+    }
+    
+    public function cancel() {
+    }
+    
+    private function postClone(event:UIEvent) {
+    }
+}
