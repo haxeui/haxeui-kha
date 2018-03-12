b12e6e18252bbfc7d98aa07b2cfc31a0d8e86dff
Pablo Martin <theawakening@gmail.com>
2018-03-12 11:15:59 +0100

    Fix.

--- haxe/ui/backend/CallLaterBase.hx
+++ haxe/ui/backend/CallLaterBase.hx
@@ -4,8 +4,6 @@ import kha.Scheduler;
 
 class CallLaterBase {
     public function new(fn:Void->Void) {
-        haxe.Timer.delay(function() {
-            Scheduler.addTimeTask(fn, 0);
-        }, 1); //Avoids infinite loop execution in `Kha.Scheduler.executeTimeTasks` if `callLater` is called from another callLater's callback
+        Scheduler.addTimeTask(fn, 0.001);
     }
 }
