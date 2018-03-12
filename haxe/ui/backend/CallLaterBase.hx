4392e79629563fdd1bb0432c21283b7ccfc433dd
Pablo Martin <theawakening@gmail.com>
2018-03-12 10:58:41 +0100

    Fix.

--- haxe/ui/backend/CallLaterBase.hx
+++ haxe/ui/backend/CallLaterBase.hx
@@ -2,11 +2,10 @@ package haxe.ui.backend;
 
 import kha.Scheduler;
 
-class CallLaterBase extends TimerBase {
+class CallLaterBase {
     public function new(fn:Void->Void) {
-        super(0, function() {
-            stop();
-            fn();
-        });
+        haxe.Timer.delay(function() {
+            Scheduler.addTimeTask(fn, 0);
+        }, 1); //Avoids infinite loop execution in `Kha.Scheduler.executeTimeTasks` if `callLater` is called from another callLater's callback
     }
 }
--- haxe/ui/backend/TimerBase.hx
+++ haxe/ui/backend/TimerBase.hx
@@ -7,21 +7,17 @@ class TimerBase {
     private var _stopped:Bool = false;  //Needed if the stop method is called in the callback execution
 
     public function new(delay:Int, callback:Void->Void) {
-        haxe.Timer.delay(function() {
-            _timerId = Scheduler.addBreakableTimeTaskToGroup(0, function() {
-                if (_stopped == false) {
-                    callback();
-                }
+        _timerId = Scheduler.addBreakableTimeTaskToGroup(0, function() {
+            if (_stopped == false) {
+                callback();
+            }
 
-                return !_stopped;
-            }, 0, delay / 1000);
-        }, 1);  //Avoids infinite loop execution in `Kha.Scheduler.executeTimeTasks`
+            return !_stopped;
+        }, 0, delay / 1000);
     }
 
     public function stop() {
         _stopped = true;
-        if (_timerId != -1) {
-            Scheduler.removeTimeTask(_timerId);
-        }
+        Scheduler.removeTimeTask(_timerId);
     }
 }
