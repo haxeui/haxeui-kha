ecdf450f0db8825c7c0a9974640b99ad93eeaa28
Ian Harrigan <ianharrigan@hotmail.com>
2019-01-02 10:38:06 +0100

    style changes

--- haxe/ui/backend/AppBase.hx
+++ haxe/ui/backend/AppBase.hx
@@ -27,6 +27,13 @@ class AppBase {
         var title:String = Toolkit.backendProperties.getProp("haxe.ui.kha.title", "");
         var width:Int = Toolkit.backendProperties.getPropInt("haxe.ui.kha.width", 800);
         var height:Int = Toolkit.backendProperties.getPropInt("haxe.ui.kha.height", 600);
+        
+        #if js
+        var canvas = cast(js.Browser.document.getElementById('khanvas'), js.html.CanvasElement);
+        canvas.width = width;
+        canvas.height = height;      
+        #end
+        
         _backgroudColor = ColorUtil.parseColor(Toolkit.backendProperties.getProp("haxe.ui.kha.background.color", "0xFFFFFF"));
         System.start( { title: title, width: width, height: height }, initialized);
     }
