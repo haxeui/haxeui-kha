74848beb103b60057671727d99f9e46e200fce24
David Klein <sh-dave@users.noreply.github.com>
2021-10-05 14:54:10 +0200

    inject input (#56)
    * add injectable mouse input listeners
    
    * add injectable keyboard input listener

--- haxe/ui/backend/ToolkitOptions.hx
+++ haxe/ui/backend/ToolkitOptions.hx
@@ -1,10 +1,36 @@
 package haxe.ui.backend;
 
 typedef ToolkitOptions = {
-    ?autoNotifyInput:Null<Bool>,
     ?noBatch:Null<Bool>,
     ?showFPS:Null<Bool>,
 
     // see https://github.com/Kode/Kha/wiki/Managing-Your-Assets#assets-names
     ?flattenAssetPaths:Bool,
+
+    ?mouseInput: MouseInputOptions,
+    ?keyboardInput: KeyboardInputOptions,
+}
+
+typedef MouseListenerCallback = (
+    (Int, Int, Int) -> Void,
+    (Int, Int, Int) -> Void,
+    (Int, Int, Int, Int) -> Void,
+    (Int) -> Void,
+    () -> Void
+) -> Void;
+
+typedef MouseInputOptions = {
+    final ?listen: MouseListenerCallback;
+    final ?unlisten: MouseListenerCallback;
+}
+
+typedef KeyListenerCallback = (
+    kha.input.KeyCode -> Void,
+    kha.input.KeyCode -> Void,
+    String -> Void
+) -> Void;
+
+typedef KeyboardInputOptions = {
+    final ?listen: KeyListenerCallback;
+    final ?unlisten: KeyListenerCallback;
 }
